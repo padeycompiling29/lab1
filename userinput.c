6f63ec6abe50924e82d7b84aab9c011265d864a4
padeyBercoding <padeyWhat@atlassian.com>
2020-10-24 18:55:41 +0800

    First commit from a newbie

---
+++ userinput.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+
+/* This is my second program in C*/
+
+int age;
+printf("How old are you?>");
+scanf("%d",&age);
+printf("You are %d years old",age);
+return (0);
+
+}
+
